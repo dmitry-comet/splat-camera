c58aba20e812b65435e71c27dd1ad11ab501bfab
Glitch (glitch-blank-website) <none>
2025-04-09 16:56:47 +0000

    🏢🍃 Checkpoint ./index.html:77432402/388 ./style.css:77432402/4702 ./script.js:77432402/4478

--- script.js
+++ script.js
@@ -1,80 +1,83 @@
-      var canvas = document.getElementById("renderCanvas");
+var canvas = document.getElementById("renderCanvas");
 
-      var startRenderLoop = function (engine, canvas) {
-        engine.runRenderLoop(function () {
-          if (sceneToRender && sceneToRender.activeCamera) {
-            sceneToRender.render();
-          }
-        });
-      };
+var startRenderLoop = function (engine, canvas) {
+  engine.runRenderLoop(function () {
+    if (sceneToRender && sceneToRender.activeCamera) {
+      sceneToRender.render();
+    }
+  });
+};
 
-      var engine = null;
-      var scene = null;
-      var sceneToRender = null;
-      var createDefaultEngine = function () {
-        return new BABYLON.Engine(canvas, true, {
-          preserveDrawingBuffer: true,
-          stencil: true,
-          disableWebGL2Support: false,
-        });
-      };
-      var createScene = function () {
-        // This creates a basic Babylon Scene object (non-mesh)
-        var scene = new BABYLON.Scene(engine);
+var engine = null;
+var scene = null;
+var sceneToRender = null;
+var createDefaultEngine = function () {
+  return new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+    disableWebGL2Support: false,
+  });
+};
+var createScene = function () {
+  // This creates a basic Babylon Scene object (non-mesh)
+  var scene = new BABYLON.Scene(engine);
 
-        var camera = new BABYLON.ArcRotateCamera(
-          "camera",
-          0,
-          1,
-          10,
-          BABYLON.Vector3.Zero(),
-          scene
-        );
-        camera.attachControl(canvas, true);
+  var camera = new BABYLON.ArcRotateCamera(
+    "camera",
+    0,
+    1,
+    10,
+    BABYLON.Vector3.Zero(),
+    scene
+  );
+  camera.attachControl(canvas, true);
 
-        // This attaches the camera to the canvas
-        camera.attachControl(canvas, true);
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new BABYLON.HemisphericLight(
-          "light",
-          new BABYLON.Vector3(0, 1, 0),
-          scene
-        );
+  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+  var light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  );
 
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.7;
 
-        BABYLON.ImportMeshAsync("https://cdn.glitch.me/55dd5f2f-fec4-4917-ab9b-93997457dadd/MKT200_2.splat?v=1744215332160", scene);
+  BABYLON.ImportMeshAsync(
+    "https://cdn.glitch.me/55dd5f2f-fec4-4917-ab9b-93997457dadd/MKT200_2.splat?v=1744215332160",
+    scene
+  );
 
-        return scene;
-      };
-      window.initFunction = async function () {
-        var asyncEngineCreation = async function () {
-          try {
-            return createDefaultEngine();
-          } catch (e) {
-            console.log(
-              "the available createEngine function failed. Creating the default engine instead"
-            );
-            return createDefaultEngine();
-          }
-        };
+  return scene;
+};
+window.initFunction = async function () {
+  var asyncEngineCreation = async function () {
+    try {
+      return createDefaultEngine();
+    } catch (e) {
+      console.log(
+        "the available createEngine function failed. Creating the default engine instead"
+      );
+      return createDefaultEngine();
+    }
+  };
 
-        window.engine = await asyncEngineCreation();
+  window.engine = await asyncEngineCreation();
 
-        const engineOptions = window.engine.getCreationOptions?.();
-        if (!engineOptions || engineOptions.audioEngine !== false) {
-        }
-        if (!engine) throw "engine should not be null.";
-        startRenderLoop(engine, canvas);
-        window.scene = createScene();
-      };
-      initFunction().then(() => {
-        sceneToRender = scene;
-      });
+  const engineOptions = window.engine.getCreationOptions?.();
+  if (!engineOptions || engineOptions.audioEngine !== false) {
+  }
+  if (!engine) throw "engine should not be null.";
+  startRenderLoop(engine, canvas);
+  window.scene = createScene();
+};
+initFunction().then(() => {
+  sceneToRender = scene;
+});
 
-      // Resize
-      window.addEventListener("resize", function () {
-        engine.resize();
-      });
+// Resize
+window.addEventListener("resize", function () {
+  engine.resize();
+});
